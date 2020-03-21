0a5c7f2c9a8ca4158c10dd8a8185fd9f70662a6f
Vadim <varem99@mail.ru>
2020-03-21 22:07:36 +0300

    Issue 2 resolved

--- Sinc/mysinc.m
+++ Sinc/mysinc.m
@@ -1,4 +1,17 @@
 function sinc = mysinc(x, a)
+    % new variable A is to be set by a or 0
+    A = 0;
+    switch nargin
+        case 1
+            A = 0;
+        case 2
+            A = a;
+        otherwise
+            error(['Invalid number of arguments' ...
+                ' in function mysinc']);
+    end
+            
+    
     % get array info
     [dim, data_range] = size(x);
     
@@ -8,10 +21,10 @@ function sinc = mysinc(x, a)
     % preventing extra initialization
     sinc = zeros(data_range, 1);
     for s = 1: 1: data_range
-        if(x(s)-a == 0)
+        if(x(s) - A == 0)
             sinc(s) = 1;
         else
-            sinc(s) = sin(x(s) - a) / (x(s) - a);
+            sinc(s) = sin(x(s) - A) / (x(s) - A);
         end
     end
 end
