faa5744b3c490c6c42c051cb74e09fbd39ec4638
Vadim <varem99@mail.ru>
2020-03-21 21:23:08 +0300

    Resolving issues 1 & 3

--- Sinc/mysinc.m
+++ Sinc/mysinc.m
@@ -1,6 +1,13 @@
 function sinc = mysinc(x, a)
-    sinc = zeros(401, 1);
-    for s = 1: 1: 401
+    % get array info
+    [dim, data_range] = size(x);
+    
+    disp(dim);
+    disp(data_range);
+    
+    % preventing extra initialization
+    sinc = zeros(data_range, 1);
+    for s = 1: 1: data_range
         if(x(s)-a == 0)
             sinc(s) = 1;
         else
--- Sinc/task3.m
+++ Sinc/task3.m
@@ -1,20 +1,14 @@
-c = -20;
-d = 20;
-step = 0.1;
-x = (c:step:d);
-a = 1;
-% disp(x);
-% number of data points
-data_range = (d-c) / step + 1;
-disp(data_range);
+% input values
+c    = -20;
+d    = 20;
+step = 0.01;
+a    = 1;
 
+% array of X points
+x = (c:step:d);
+
+% array of sinc(x) values
 y = mysinc(x, a);
 
-% y = zeros(data_range, 1);
-% %disp(y);
-% for s = 1: 1: data_range
-%    y(s) = mysinc(x(s), a);
-% end
-
-%disp(y);
+% build graphs
 plot(x, y);
