a9677cdbfb536adb3af8973b6e4e4f612ed643bd
Vadim <varem99@mail.ru>
2020-03-22 20:04:52 +0300

    Graph Builder

--- Graph_builder/task4.m
+++ Graph_builder/task4.m
@@ -4,14 +4,30 @@
 c    = 0.01;
 d    = 100;
 step = 0.01;
-k = [1; 2];
-disp(k);
-% k = 1;
-
+k = [1; 2; 3; 4; 5];
+k_size = size(k);
+% disp(k);
 
 % array of X points
 x = (c:step:d);
 
 y = Graph_builder(k, x);
 
-plot(x, y);
+fig = plot(x, y, 'LineWidth', 3);
+axis([0.01 2 -5 2]);
+
+str_arr = string(k);
+for i = 1:k_size
+    str_arr(i) = 'Line ' + str_arr(i) + ' - k = ' + str_arr(i);
+end
+disp(str_arr);
+lgd = legend(str_arr, 'Location','southeast');
+lgd.FontSize = 12;
+
+
+grid on;
+title('Y = k * lnX')
+xlabel('x')
+ylabel('y')
+
+print('Graphs.png','-dpng','-r300');  
