da5992e80b00a1066f6ddbf5aea0203a67fbc813
Vadim <varem99@mail.ru>
2020-03-24 20:43:16 +0300

    Legend building issue resolved

--- 4_Graph_Builder/task4.m
+++ 4_Graph_Builder/task4.m
@@ -4,7 +4,7 @@
 c    = 0.01;
 d    = 100;
 step = 0.01;
-k = [1; 2; 3; 4; 5];
+k = (-1 + (7)*(rand(1,randi([1,8])))).';
 k_size = size(k);
 % disp(k);
 
@@ -18,7 +18,7 @@ axis([0.01 2 -5 2]);
 
 str_arr = string(k);
 for i = 1:k_size
-    str_arr(i) = 'Line ' + str_arr(i) + ' - k = ' + str_arr(i);
+    str_arr(i) = ' - k = ' + str_arr(i);
 end
 disp(str_arr);
 lgd = legend(str_arr, 'Location','southeast');
