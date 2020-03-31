93028c2c87b16a1f4842420c5054d038323b26f2
Vadim <varem99@mail.ru>
2020-03-31 11:35:45 +0300

    Graph vuilder

---
+++ Data_analysis/Graph_bulder.m
@@ -0,0 +1,25 @@
+% graph builder
+function y = Graph_builder(k, x)
+    y = k.* log(x);
+    
+    plot(x, y, 'LineWidth', 3);
+    axis([0.01 2 -5 2]);
+    
+    k_size = size(k);
+    
+    str_arr = string(k);
+    for i = 1:k_size
+        str_arr(i) = ' - k = ' + str_arr(i);
+    end
+    disp(str_arr);
+    lgd = legend(str_arr, 'Location','southeast');
+    lgd.FontSize = 12;
+    
+    
+    grid on;
+    title('Y = k * lnX')
+    xlabel('x')
+    ylabel('y')
+    
+    print('Graphs.png','-dpng','-r300');  
+end
