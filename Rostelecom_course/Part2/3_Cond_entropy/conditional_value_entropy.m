8f9433cdb9a50e6434cf4c43637608995a4c0782
Vadim <varem99@mail.ru>
2020-03-31 23:02:18 +0300

    algorithm improved

--- Rostelecom_course/Part2/3_Cond_entropy/conditional_value_entropy.m
+++ Rostelecom_course/Part2/3_Cond_entropy/conditional_value_entropy.m
@@ -4,6 +4,7 @@ function condv_ent = conditional_value_entropy(X, Y, y)
        disp('y is not contained in Y');
        return;
    end
+   
    %format long;
    % disp(X);
    % disp(Y);
@@ -11,13 +12,11 @@ function condv_ent = conditional_value_entropy(X, Y, y)
    alp_prob_X = alphabet_probabilities(X);
    %alp_prob_Y = alphabet_probabilities(Y);
    alp_size = size(alp_prob_X{1,2});
-   %condv_ent = -sum();
-   %a = (alp_prob{1,2}(2));
-   %p_a_b = (length(find(and((X == 'a'), (Y == 'b')))) / X_size(2)) / (alp_prob_Y{1,2}(2));
+    
    condv_ent = 0;
    for i = 1:alp_size(2)
        %p_a_b = (length(find(and((X == alp_prob_X{1, 2}(i)), (Y == alp_prob_Y{1, 2}(i))))) / X_size(2)) ./ (alp_prob_Y{1,2}(i));
-       p_a_b = (length(find(and((X == alp_prob_X{1, 1}(i)), (Y == y)))) / X_size(2)) / (length(find(Y == y)) / X_size(2));
+       p_a_b = (length(find(and((X == alp_prob_X{1, 1}(i)), (Y == y)))) ) / (length(find(Y == y)) );
        % disp(p_a_b);
        if(p_a_b == 0)
            continue;
