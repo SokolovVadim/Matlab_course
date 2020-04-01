81304007c5639c9e9e0532ba28ab84b6afc4a915
Vadim <varem99@mail.ru>
2020-04-01 13:58:58 +0300

    Equation checked

--- Rostelecom_course/Part2/6_Equation_check/task6.m
+++ Rostelecom_course/Part2/6_Equation_check/task6.m
@@ -28,3 +28,36 @@ disp(inf_XY);
 % I(Y;X)
 inf_YX = information(Y, X);
 disp(inf_XY);
+
+% Equation check
+L = length(X);
+% 1) H(Y) <= H(X) <= L
+if(and((ent_Y <= ent_X), (ent_X <= L)))
+    disp("1) correct");
+else
+    disp("1) incorrect");
+end
+% 2) H(X) <= H(Y) <= L
+if(and((ent_X <= ent_Y), (ent_Y <= L)))
+    disp("2) correct");
+else
+    disp("2) incorrect");
+end
+% 3) I(X;Y) == I(Y;X)
+if(inf_XY == inf_YX)
+    disp("3) correct");
+else
+    disp("3) incorrect");
+end
+% 4) H(XY) = H(X) + H(X)
+if(ent_XY == ent_X + ent_X)
+    disp("4) correct");
+else
+    disp("4) incorrect");
+end
+% 5) H(XY) = H(Y) + H(Y)
+if(ent_XY == ent_Y + ent_Y)
+    disp("5) correct");
+else
+    disp("5) incorrect");
+end
