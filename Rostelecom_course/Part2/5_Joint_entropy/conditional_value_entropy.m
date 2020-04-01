d1c8c9bfde3f418188e86cff36e5761ffd3016db
Vadim <varem99@mail.ru>
2020-04-01 23:20:40 +0300

    cond_val_ent add

---
+++ Rostelecom_course/Part2/5_Joint_entropy/conditional_value_entropy.m
@@ -0,0 +1,28 @@
+function condv_ent = conditional_value_entropy(X, Y, y)
+   if(isempty(find(Y == y)))
+       condv_ent = -1;
+       disp('y is not contained in Y');
+       return;
+   end
+   
+   %format long;
+   % disp(X);
+   % disp(Y);
+   X_size = size(X);
+   alp_prob_X = alphabet_probabilities(X);
+   %alp_prob_Y = alphabet_probabilities(Y);
+   alp_size = size(alp_prob_X{1,2});
+    
+   condv_ent = 0;
+   for i = 1:alp_size(2)
+       %p_a_b = (length(find(and((X == alp_prob_X{1, 2}(i)), (Y == alp_prob_Y{1, 2}(i))))) / X_size(2)) ./ (alp_prob_Y{1,2}(i));
+       p_a_b = (length(find(and((X == alp_prob_X{1, 1}(i)), (Y == y)))) ) / (length(find(Y == y)) );
+       % disp(p_a_b);
+       if(p_a_b == 0)
+           continue;
+       end
+       condv_ent = condv_ent - p_a_b * log2(p_a_b);
+   end
+   % ent = (length(find(and((X == alp_prob_X{1, 2}), (Y == alp_prob_Y{1, 2})))) / X_size(2)) ./ (alp_prob{1,2});
+   % disp(condv_ent);
+end
