a65bae2916df7946a801e46e6b63f120055000b2
Vadim <varem99@mail.ru>
2020-04-07 19:25:00 +0300

    cond ent joint ent

--- Rostelecom_course/Part2/4_Cond_entropy/conditional_entropy.m
+++ Rostelecom_course/Part2/4_Cond_entropy/conditional_entropy.m
@@ -1,13 +1,11 @@
 function ent = conditional_entropy(X, Y)
-    alph = unique(Y);
-    
+    [alph, prob] = alphabet_probabilities(Y);
     alph_size = size(alph);
     cond_ent = zeros(alph_size);
     
-    alph_prob_Y = alphabet_probabilities(Y);
     ent = 0;
     for i = 1:alph_size(2)
         cond_ent(i) = conditional_value_entropy(X, Y, alph(i));
-        ent = ent + alph_prob_Y{1, 2}(i) * cond_ent(i);
+        ent = ent + prob(i) * cond_ent(i);
     end
 end
--- Rostelecom_course/Part2/5_Joint_entropy/joint_entropy.m
+++ Rostelecom_course/Part2/5_Joint_entropy/joint_entropy.m
@@ -1,5 +1,5 @@
 function j_ent = joint_entropy(X, Y)
-     % j_ent = entropy(Y) + conditional_entropy(X, Y);
+    % j_ent = entropy(Y) + conditional_entropy(X, Y);
     tuple = string([X' Y'])';
     j_ent = entropy(tuple);
 end
