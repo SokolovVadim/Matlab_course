bec13d9c3d8e9ba7536ddb60cb75f438fd8478d6
Vadim <varem99@mail.ru>
2020-04-07 16:44:58 +0300

    Entropy refactor

--- Rostelecom_course/Part2/2_Entropy/alphabet_probabilities.m
+++ Rostelecom_course/Part2/2_Entropy/alphabet_probabilities.m
@@ -1,18 +1,14 @@
-function alph = alphabet_probabilities(X)
-    alph = cell(1, 2);
-    
+function [alph, prob] = alphabet_probabilities(X) 
     X_size = size(X);
     symbolic_array = unique(X);
     
-    array_size = size(symbolic_array);
-    % disp(symbolic_array);
+    array_size = size(symbolic_array, 2);
+    prob_array = zeros(1, array_size);
     
-    prob_array = zeros(array_size);
-    
-    for i = 1:array_size(2)
+    for i = 1:4
         prob_array(i) = length(find(X == symbolic_array(i))) / X_size(2);
     end
     
-    alph{1, 1} = symbolic_array;
-    alph{1, 2} = prob_array;
+    alph = symbolic_array;
+    prob = prob_array;
 end
