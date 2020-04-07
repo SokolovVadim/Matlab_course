3dfe98ed2c2b34fbc2014a5bb07667569f6de5bc
Vadim <varem99@mail.ru>
2020-04-07 19:20:44 +0300

    alph prob

--- Rostelecom_course/Part2/1_Alphabet/alphabet_probabilities.m
+++ Rostelecom_course/Part2/1_Alphabet/alphabet_probabilities.m
@@ -5,7 +5,7 @@ function [alph, prob] = alphabet_probabilities(X)
     array_size = size(symbolic_array, 2);
     prob_array = zeros(1, array_size);
     
-    for i = 1:4
+    for i = 1:array_size
         prob_array(i) = length(find(X == symbolic_array(i))) / X_size(2);
     end
     
