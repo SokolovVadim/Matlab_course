3f5ef575e08bbef1ee59181811927376e1b51a70
Vadim <varem99@mail.ru>
2020-04-18 16:38:20 +0300

    task 14 uod

--- Digital_Handling_Lab/Task1/task_14.m
+++ Digital_Handling_Lab/Task1/task_14.m
@@ -1,6 +1,7 @@
 % Sokolov Vadim, 716 group
 % Task 14
 % orthonormal issue resolved
+
 matrix = [1/16, 1/16, 1/16, 1/16, 1/8, 1/8, 1/8, 1/8;
             1/8, 1/8, 1/8, 1/8, -1/16, -1/16, -1/16,-1/16;
             1/4, 1/4, -1/4, -1/4, 0, 0, 0, 0;
@@ -14,12 +15,11 @@ matrix = [1/16, 1/16, 1/16, 1/16, 1/8, 1/8, 1/8, 1/8;
 %         1, 2;
 %         2, 1
 % ];
-
+        
 disp(matrix);
 
 m_size = size(matrix);
 m_size = m_size(1);
-disp(m_size);
 
 % Numeric format option
 format long;
@@ -44,5 +44,13 @@ disp(r);
 Q = orth(matrix);
 disp('orthonormal basis');
 disp(Q);
+disp('Rank of matrix Q consisting of orthonormal vectors:');
 disp(rank(Q)); % rank is equal to rank of matrix 
+
+disp('Q`*Q');
+disp(Q'*Q);
+
+N = norm(eye(r)-Q'*Q,'fro');
+disp('Norm:');
+disp(N);
 
